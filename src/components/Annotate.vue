3da20455300971cf82da98e44cfac3b7bb22161b
lyine <liuzhengyuan@hotmail.com>
2022-04-21 22:29:47 +0800

    chore: use svg file

--- src/components/Annotate.vue
+++ src/components/Annotate.vue
@@ -17,20 +17,10 @@
         v-if="!edit && editPermit"
         v-on:click="enterEdit"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-          <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-          <path
-            d="M362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32zM421.7 220.3L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3z"
-          />
-        </svg>
+        <img src="../assets/edit.svg" />
       </button>
       <button class="edit-btn cancel-btn" v-if="edit" v-on:click="cancelEdit">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-          <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-          <path
-            d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"
-          />
-        </svg>
+        <img src="../assets/cross.svg" />
         取消编辑
       </button>
       <button class="edit-btn save-btn" v-if="edit" v-on:click="saveEdit">
@@ -144,7 +134,7 @@ $on-background: map-get(theme.$palette, "on-background");
         background-color: theme.darken($background, 5);
         cursor: pointer;
       }
-      svg {
+      img {
         position: relative;
         top: 4px;
         float: left;
@@ -155,24 +145,21 @@ $on-background: map-get(theme.$palette, "on-background");
     }
     .edit {
       position: relative;
-      top: -0.3em;
       height: 1.3em;
-      background-color: inherit;
+      background: transparent;
       color: $primary;
       padding: 0;
       @media (hover: hover) {
         opacity: 0; // hide edit icon when device can hover
       }
       &:hover {
-        background-color: inherit;
-        svg {
-          fill: theme.darken($background, 8);
-        }
+        background: transparent;
+        opacity: 1;
       }
-      svg {
+      img {
         margin-right: 0;
-        height: 100%;
-        fill: theme.darken($background, 5);
+        height: 80%;
+        opacity: 0.6;
       }
     }
     .save-btn {
@@ -192,7 +179,7 @@ $on-background: map-get(theme.$palette, "on-background");
   &:hover {
     @media (hover: hover) {
       .edit {
-        opacity: 1; // show edit icon when hover to anntation
+        opacity: 0.7; // show edit icon when hover to anntation
       }
     }
     .quote-content {
