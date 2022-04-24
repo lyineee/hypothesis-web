0bc9e13ae18f5b0c61c1700d8ff26f21e9bbc09d
lyine <liuzhengyuan@hotmail.com>
2022-04-25 01:04:27 +0800

    feat: dark mode with system

--- src/components/ThemeService.ts
+++ src/components/ThemeService.ts
@@ -1,23 +1,72 @@
 
 
 export default class ThemeService {
-    private currentTheme = "light"
-    private autoTheme = false
-    
-    set theme(theme: string) {
+    private currentTheme: ThemeString = "light"
+    private systemTheme!: ThemeString
+    private setting!: ThemeSetting
+    private settingStore = "themeSetting"
+
+    private set theme(theme: ThemeString) {
         this.currentTheme = theme
         document.documentElement.setAttribute("data-theme", theme);
     }
     get theme() {
+        return this.currentTheme as ThemeString
+    }
+
+    auto() {
+        this.setTheme()
+    }
+
+    // user set theme
+    setTheme(theme?: ThemeString) {
+        if (theme) {
+            this.theme = theme
+            this.setting.theme = theme
+        } else {
+            this.setting.theme = undefined
+        }
+        this.updateSetting()
+    }
+
+    getTheme(): ThemeString {
         return this.currentTheme
     }
 
     getSystemTheme(): string {
-        return "dark"
+        return this.systemTheme
+    }
+
+    getSetting() {
+        return this.setting
+    }
+
+    private updateSetting() {
+        localStorage.setItem(this.settingStore, JSON.stringify(this.setting))
+    }
+
+    private onQueryChange(ev: MediaQueryListEvent) {
+        this.systemTheme = ev.matches ? "dark" : "light"
+        if (this.setting.theme) {
+            this.theme = this.systemTheme
+        }
     }
 
     constructor() {
-        // TODO get setting from localstorage
-        // TODO media listener
+        const setting = localStorage.getItem(this.settingStore)
+        if (setting) {
+            this.setting = JSON.parse(setting)
+        } else {
+            this.setting = {}
+        }
+        const darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+        this.systemTheme = darkQuery.matches ? "dark" : "light"
+        darkQuery.addEventListener("change", (() => { return this.onQueryChange }))
     }
 }
+
+interface ThemeSetting {
+    theme?: ThemeString
+}
+
+type ThemeString = "light" | "dark"
